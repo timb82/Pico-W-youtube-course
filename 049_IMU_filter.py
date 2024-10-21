2536cdce9be50bdddf32beec72aa16f6ec59fb9a
Michal Lazarczyk <michal.lazarczyk@technipfmc.com>
2024-10-21 12:46:58 +0200

    add complimentary filter

--- 049_IMU_filter.py
+++ 049_IMU_filter.py
@@ -15,30 +15,53 @@ pitchG = 0
 rollG = 0
 roll_comp = 0
 pitch_comp = 0
-# yawG = 0
+yawG = 0
 dt = 0
 cntr = 0
-conf = 0.1
+conf = 0.05
+
+#calibration coefficients
+x_offset = 0.0172493133544921875
+y_offset = -1.535597393798828125      #-1.530371189117431640625000
+z_offset = 0.7726116862487792968750   #0.769709777832031250000000
+callibration = False   # Set to True to get callibration coefficients
+if callibration:
+    t0=ticks_ms()
+    cal_time=60
 
 while True:
-    t_start = ticks_ms()
-    gx = mpu.gyro.x  - 0.05022978
-    gy = -mpu.gyro.y  + 1.5604778
-    # gz = mpu.gyro.z - 0.799  # 0.8015522
-    ax = mpu.accel.x
-    ay = mpu.accel.y
-    az = mpu.accel.z
-    rollG = rollG + gy * dt
-    pitchG = pitchG + gx * dt
-    # yawG = yawG + gz * dt
-    if az!=0:
-        rollA = atan(ax / az) / 2 / pi * 360
-        pitchA = atan(ay / az) / 2 / pi * 360
-        roll_comp = rollA * conf + (1-conf) * (roll_comp + gy*dt)
-        pitch_comp = pitchA * conf + (1-conf) * (pitch_comp + gx*dt)
-    cntr += 1
-    if cntr == 10:
-        print(f"RG={rollG}, PG={pitchG}, Rc={roll_comp}, Pc={pitch_comp}")
-        cntr = 0
-    t_stop = ticks_ms()
-    dt = (t_stop - t_start) / 1000
+    try:
+        t_start = ticks_ms()
+        gx = mpu.gyro.x  - x_offset
+        gy = -mpu.gyro.y  - y_offset
+        gz = mpu.gyro.z - z_offset
+        ax = mpu.accel.x
+        ay = mpu.accel.y
+        az = mpu.accel.z
+        pitchG = pitchG + gx * dt
+        rollG = rollG + gy * dt
+        yawG = yawG + gz * dt
+        if az!=0:
+            rollA = atan(ax / az) / 2 / pi * 360
+            pitchA = atan(ay / az) / 2 / pi * 360
+            roll_comp = rollA * conf + (1-conf) * (roll_comp + gy*dt)
+            pitch_comp = pitchA * conf + (1-conf) * (pitch_comp + gx*dt)
+        cntr += 1
+        if cntr == 10:
+            if callibration:
+                print(f"x={pitchG}, y={rollG}, z={yawG}")
+                if (ticks_ms()-t0)/1000 >cal_time:
+                    raise KeyboardInterrupt
+            else:
+                print(f"RG={rollG}, PG={pitchG}, Rc={roll_comp}, Pc={pitch_comp}, Y={yawG}")
+            cntr = 0
+        t_stop = ticks_ms()
+        dt = (t_stop - t_start) / 1000
+    except KeyboardInterrupt:
+        if callibration:
+            Ax,Ay,Az = pitchG, rollG, yawG
+            t = (ticks_ms()-t0)/1000
+            print(f"Ellapsed time: {t:.2f}\n callibration values:")
+            print(f"x_off = {x_offset + Ax/t: .22f}\ny_off = {y_offset + Ay/t: .22f}\nz_off = {z_offset+ Az/t: .22f}")
+            print(f"\n dx = {Ax/t: .22f}\ndy = {Ay/t: .22f}\ndz = {Az/t: .22f}")
+        break
