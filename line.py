aaa2cc199ceb83f83b6ccbd7963b3014a55dbe11
Michal Lazarczyk <michal.lazarczyk@technipfmc.com>
2024-10-22 15:31:14 +0200

    add linear function coefficients calculator

---
+++ line.py
@@ -0,0 +1,12 @@
+x1 = 0
+y1 = 5
+x2 = 180
+y2 = 25
+# x1 = float(input("x1= "))
+# y1 = float(input("y1= "))
+# x2 = float(input("x2= "))
+# y2 = float(input("y2= "))
+
+a = (y2 - y1) / (x2 - x1)
+b = y1 - x1 * a
+print(f"y = {a:.32f} x + {b:.32f}")
