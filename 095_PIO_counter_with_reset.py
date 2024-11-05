ee3a29f4153b475606e39de58f563b3aafd3fd40
timb82 <25963560+timb82@users.noreply.github.com>
2024-11-05 22:27:33 +0100

    minor updates

--- 095_PIO_counter_with_reset.py
+++ 095_PIO_counter_with_reset.py
@@ -1,3 +1,5 @@
+# type: ignore   // for Pylance & ruff to ignore assembly
+# ruff: noqa
 import rp2
 from machine import Pin
 
@@ -14,6 +16,7 @@ def bin_counter():
     mov(pins, osr)
     wrap_target()
 
+    # loop while waiting for button press
     label("read_again")
     mov(isr, null)
     in_(pins, 2)
@@ -22,18 +25,20 @@ def bin_counter():
     mov(x, isr)
     jmp(not_x, "read_again")
 
-    # red button
+    # red button // bin counter +1
     set(y, 0b0010)
     jmp(x_not_y, "check_green")
     wait(0, pin, 1)
+    # x + 1 == !(!x-1)
     mov(x, invert(osr))
     jmp(x_dec, "set_pins")
 
-    # green button
+    # green button // reset
     label("check_green")
     set(y, 0b0001)
     jmp(x_not_y, "read_again")
     wait(0, pin, 0)
+    # x = 0  == !x = 0b1111
     set(x, 0b1111)
 
     label("set_pins")
@@ -45,5 +50,6 @@ def bin_counter():
 sm0 = rp2.StateMachine(0, bin_counter, freq=0, out_base=Pin(PIN1), in_base=pin_sw1)
 sm0.active(1)
 
+# required for keeping SM alive in vs code, can be omitted when using Thonny
 while True:
     pass
